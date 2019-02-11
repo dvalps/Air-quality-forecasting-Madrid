71856b36bdfdb7c145631380fcd0e7106dbe6a8a
Damir <dvalpy@gmail.com>
2019-02-11 17:44:28 +0100

    finished meta

--- meta.py
+++ meta.py
@@ -14,10 +14,10 @@ import pandas as pd
 from evaluate_forecast import evaluate_forecast
 
 horizons = [1, 2, 6, 12, 24, 48]
-models = ['FNN_deep1', 'FNN_deep2', 'FNN_deep3', 'SVM', 'RF', 'lstm1'] #'LinReg',
+models = ['FNN_deep1', 'FNN_deep2', 'FNN_deep3', 'SVM', 'RF', 'lstm1'] #'LinReg' is still first
 
 
-for h in horizons:
+for h in horizons[0:1]:
     # read the data from csv
     df = pd.read_csv('forecasts/forecasts_t' + str(h) + "_" + 'LinReg.csv', usecols = ['observed', 'predictions'])
     #print(df.head())
@@ -36,9 +36,14 @@ print(df.head())
 rmse_avg, mae_avg, ia_avg, mb_avg, pears_avg = evaluate_forecast(df.observed, df.avg, normalize=0)
 
 # Calculate weighted average
-pass
+"""Order: LinReg, FNN, FNN 2 layers, ... follow the order in models
 """
-TO DO
-1 - (WEIGHT / WEIGHT.sum()), weights = RMSE_test
-"""
+
+rmses = [5, 10, 20, 15, 3, 5, 8]
+weights = (1 / (len(rmses) - 1)) * (1 - rmses / np.sum(rmses))
+
+df['avg_weighted'] = np.average(np.array(df.iloc[:,1:len(models) + 2]), axis = 1, weights = weights)
+print(df.head())
+
+rmse_avg_w, mae_avg_w, ia_avg_w, mb_avg_w, pears_avg_w = evaluate_forecast(df.observed, df.avg_weighted, normalize=0)
 
