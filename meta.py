b2409ee8b5768b7f168c5cf22b1bfe271c251291
Damir Valput <dvalpy@gmail.com>
2019-02-12 01:09:50 +0100

    meta done

--- meta.py
+++ meta.py
@@ -14,10 +14,14 @@ import pandas as pd
 from evaluate_forecast import evaluate_forecast
 
 horizons = [1, 2, 6, 12, 24, 48]
-models = ['FNN_deep1', 'FNN_deep2', 'FNN_deep3', 'SVM', 'RF', 'lstm1'] #'LinReg' is still first
+models = ['FNN_deep1', 'FNN_deep2', 'FNN_deep3', 'SVM', 'RF', 'lstm1', 'DeepLSTM', 'CNN'] #'LinReg' is still first
 
+error_meta = pd.DataFrame(columns = horizons)
+"""
+Order of rows in this data frame is: rmse and mb for average, rmse and mb for weighted average.
+"""
 
-for h in horizons[0:1]:
+for h in horizons:
     # read the data from csv
     df = pd.read_csv('forecasts/forecasts_t' + str(h) + "_" + 'LinReg.csv', usecols = ['observed', 'predictions'])
     #print(df.head())
@@ -27,23 +31,28 @@ for h in horizons[0:1]:
         # add a predictions column - using the name of the model as appendix
         df["predictions_" + m] = df_temp["predictions"]
         
+    # calculate average
+    df['avg'] = np.average(np.array(df.iloc[:,1:len(models) + 2]), axis = 1)
+    
+    rmse_avg, mae_avg, ia_avg, mb_avg, pears_avg = evaluate_forecast(df.observed, df.avg, normalize=0)
+    
+    
+    # jer mi se ne da jebati, za sve horizonte sam uzeo rmse za t = 12 -- odlucih da je
+    # aproksimacija dobra za sve vise manje.
+    rmses = [17.79, 15.54, 15.27, 15.10, 18.03, 15.86, 15.31, 15.27, 15.65]
+    weights = (1 / (len(rmses) - 1)) * (1 - rmses / np.sum(rmses))
 
+    df['avg_weighted'] = np.average(np.array(df.iloc[:,1:len(models) + 2]), axis = 1, weights = weights)
+    rmse_avg_w, mae_avg_w, ia_avg_w, mb_avg_w, pears_avg_w = evaluate_forecast(df.observed, df.avg_weighted, normalize=0)
 
-# calculate average
-df['avg'] = df.mean(axis = 1, skipna=True)
-print(df.head())
-
-rmse_avg, mae_avg, ia_avg, mb_avg, pears_avg = evaluate_forecast(df.observed, df.avg, normalize=0)
+    error_meta[h] = [rmse_avg, mb_avg, rmse_avg_w, mb_avg_w]
+        
 
 # Calculate weighted average
 """Order: LinReg, FNN, FNN 2 layers, ... follow the order in models
 """
 
-rmses = [5, 10, 20, 15, 3, 5, 8]
-weights = (1 / (len(rmses) - 1)) * (1 - rmses / np.sum(rmses))
 
-df['avg_weighted'] = np.average(np.array(df.iloc[:,1:len(models) + 2]), axis = 1, weights = weights)
 print(df.head())
 
-rmse_avg_w, mae_avg_w, ia_avg_w, mb_avg_w, pears_avg_w = evaluate_forecast(df.observed, df.avg_weighted, normalize=0)
-
+print(error_meta.head())
